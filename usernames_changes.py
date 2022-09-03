4f1b7bb275332defc93f994e3c6086c56b29d732
Archit Kandu <architkandu@hotmail.com>
2022-09-04 00:46:43 +0530

    All Done

--- usernames_changes.py
+++ usernames_changes.py
@@ -1,19 +1,8 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-
-#
-# Complete the 'possibleChanges' function below.
-#
-# The function is expected to return a STRING_ARRAY.
-# The function accepts STRING_ARRAY usernames as parameter.
-#
-
 def possibleChanges(usernames):
     ans = []
     for u in usernames:
@@ -28,18 +17,12 @@ def possibleChanges(usernames):
     return ans
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     usernames_count = int(input().strip())
-
     usernames = []
-
     for _ in range(usernames_count):
         usernames_item = input()
         usernames.append(usernames_item)
-
     result = possibleChanges(usernames)
-
     fptr.write('\n'.join(result))
     fptr.write('\n')
-
     fptr.close()
